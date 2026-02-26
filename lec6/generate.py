ffd1d4d48e467ce3eb13b07f557427cc2cdcff99
Vladyslav Paliichuk <palijczukvladyslav@gmail.com>
2026-02-27 01:25:41 +0200

    fix generate.py

--- lec6/generate.py
+++ lec6/generate.py
@@ -28,8 +28,8 @@ contents_tpl = env.get_template("contents.html.j2")
 
 # generate slides
 for i, s in enumerate(slides):
-    prev = "contents.html.j2" if i == 0 else slides[i-1]["file"]
-    next = "contents.html.j2" if i == len(slides)-1 else slides[i+1]["file"]
+    prev = "index.html" if i == 0 else slides[i-1]["file"]
+    next = "index.html" if i == len(slides)-1 else slides[i+1]["file"]
 
     html = slide_tpl.render(slide=s, prev=prev, next=next)
     (OUT / s["file"]).write_text(html, encoding="utf-8")
